45b1f662031e7d578b003c212223e917ad14e39d
vosure <vosure1@gmail.com>
2018-09-22 16:31:16 +0300

    added raster and array of pixels

--- src/com/vosure/game/Game.java
+++ src/com/vosure/game/Game.java
@@ -3,6 +3,8 @@ package com.vosure.game;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 public class Game extends Canvas implements Runnable {
 
@@ -13,6 +15,9 @@ public class Game extends Canvas implements Runnable {
     private Thread thread;
     private boolean running;
     private JFrame frame;
+
+    private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
     public Game(){
         Dimension size = new Dimension(width * scale, height * scale);
